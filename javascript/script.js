820fed9c96df78286b5e64f2349b2e7c79491a35
Keti Lazarashvili <evdoksiaketevan@gmail.com>
2022-10-19 19:33:03 +0300

    Second commit

--- javascript/script.js
+++ javascript/script.js
@@ -1,6 +1,31 @@
 const display = document.querySelector("#display");
 const buttons = document.querySelectorAll(".button");
 const equals = document.querySelector(".equals");
+let displayCalc = "";
 
+buttons.forEach(button => {
+    button.addEventListener("click", ()=>{
+        let btnValue = button.getAttribute("value");
 
+        if (btnValue != "clear" && btnValue !="delete") {
+            displayCalc += btnValue;
+            display.textContent = displayCalc;
+        }
+        else if (btnValue == "delete") {
+            displayCalc = displayCalc.slice(0, -1);
+            display.textContent = displayCalc;
+        }
+        else {
+            displayCalc = "";
+            display.textContent = displayCalc;
+        }
+    })
+})
 
+equals.addEventListener("click", () => {
+    try {
+        display.textContent = eval(displayCalc);
+    } catch (error) {
+        alert("That ain't happening. Don't try to break my calculator!");
+    }
+})
